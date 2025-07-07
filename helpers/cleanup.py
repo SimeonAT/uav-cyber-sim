22e3c5e08df31f8076cd36185c94f77ae72d1f09
Daniil Shatokhin <dan.shatokhin@gmail.com>
2025-07-07 10:12:31 -0500

    add log cleanup

--- helpers/cleanup.py
+++ helpers/cleanup.py
@@ -27,8 +27,16 @@ def kill_processes(victims: All | List[str] = "all"):
         os.system(f"pkill -9 -f {process}")
 
 
+def delete_logs():
+    """Delete all proxy log files."""
+    for file in os.listdir():
+        if file.startswith("proxy_") and file.endswith(".log"):
+            os.remove(file)
+
+
 def clean(victims: All | List[str] = "all", sim_out: bool = True):
     """End the simulation."""
     kill_processes(victims)
+    delete_logs()
     if sim_out and LOGS_PATH.exists():
         shutil.rmtree(LOGS_PATH)
