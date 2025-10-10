724a955e9452d08aa919df7d98654b5fb8737a1a
brycethebjorkman <46073756+brycethebjorkman@users.noreply.github.com>
2025-10-10 13:57:45 -0500

    oracle: modify retransmit_rid to use uli-net-sim instead of distance check

--- oracle.py
+++ oracle.py
@@ -7,8 +7,11 @@ Currently provides basic global position tracking and mission completion detecti
 
 from __future__ import annotations
 
+from inspect import CO_GENERATOR
+import json
 import logging
 import pickle
+import subprocess
 import threading
 import time
 from collections import defaultdict
@@ -61,6 +64,11 @@ class RID:
         """Snapshot of current ENU position."""
         with self.lock:
             return self._pos
+    
+    def data(self) -> RIDData:
+        """Snapshot of current data"""
+        with self.lock:
+            return self._data
 
     def pop_data(self) -> RIDData | None:
         """Return the pending RIDData once and clear it so it won't be resent."""
@@ -314,10 +322,63 @@ class Oracle:  # UAVMonitor
                     time.sleep(TX_LOOP_SLEEP)
                     continue
                 pkt = rid.pop_data()
+
+                # get position and velocity parameters for each drone
+                pos = pkt.enu_pos
+                spd = pkt.speed
+                cog = pkt.cog
+                ele = pkt.ele
+                operands = [
+                    f"{sysid},{round(pos.x,3)},{round(pos.y,3)},{round(pos.z,3)},{round(spd,3)},{round(cog,3)},{round(ele,3)}"
+                ]
+                o_sysids = []
                 for o_sysid in self.grid.iter_neighbors_within(
                     sysid, rid.pos(), radius=None
                 ):
-                    self.rid_out_socks[o_sysid].send_pyobj(pkt)  # type: ignore
+                    o_rid = self.grid.get_rid(o_sysid)
+                    # TODO: get this information with certainty instead of allowing None
+                    if o_rid is not None:
+                        o_sysids.append(o_sysid)
+                        o_data = o_rid.data()
+                        o_pos = o_data.enu_pos
+                        o_spd = o_data.speed
+                        o_cog = o_data.cog
+                        o_ele = o_data.ele
+                        operands.append(
+                            f"{o_sysid},{round(o_pos.x,3)},{round(o_pos.y,3)},{round(o_pos.z,3)},{round(o_spd,3)},{round(o_cog,3)},{round(o_ele,3)}"
+                        )
+
+                # continue if there not at least two drones to simulate
+                if len(operands) <= 1:
+                    continue
+
+                # invoke a one-off uli-net-sim Remote ID broadcast simulation
+                result = subprocess.run(
+                    [
+                        "./rid-one-off.sh",
+                        "-n", f"{sysid}",
+                        # TODO: fill in these RID fields later if needed
+                        "-t", "0",
+                        "-x", "0",
+                        "-y", "0",
+                        "-z", "0",
+                        "-v", "0",
+                        "-g", "0",
+                        "-h", "0",
+                        "-q",
+                        "--",
+                        *operands
+                    ],
+                    cwd="/usr/uli-net-sim",
+                    capture_output=True,
+                    text=True,
+                )
+                logging.debug(f"rid-one-off:\noperands:\n{operands}\n\nstdout:\n{result.stdout}\n\nstderr:\n{result.stderr}")
+                
+                res = json.loads(result.stdout)
+                for o_sysid in o_sysids:
+                    if sysid in res["Serial Number"][o_sysid]["values"]:
+                        self.rid_out_socks[o_sysid].send_pyobj(pkt)  # type: ignore
             except Exception as e:
                 logging.error(f"Retransmit error for {sysid}: {e}")
             time.sleep(TX_LOOP_SLEEP)
