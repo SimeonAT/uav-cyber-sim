4f042fd9d211f37c652b1e62592c4c29315ae504
brycethebjorkman <46073756+brycethebjorkman@users.noreply.github.com>
2025-10-13 10:33:08 -0500

    oracle: update retransmit_rid to use new rid data pattern

--- oracle.py
+++ oracle.py
@@ -7,8 +7,10 @@ Currently provides basic global position tracking and mission completion detecti
 
 from __future__ import annotations
 
+import json
 import logging
 import pickle
+import subprocess
 import threading
 import time
 from collections import defaultdict
@@ -280,81 +282,80 @@ class Oracle:  # UAVMonitor
                     time.sleep(TX_LOOP_SLEEP)
                     continue
                 # get position and velocity parameters for each drone
-                # pos = pkt.enu_pos
-                # spd = pkt.speed
-                # cog = pkt.cog
-                # ele = pkt.ele
-                # operands = [
-                #     (
-                #         f"{sysid},{round(pos.x, 3)},{round(pos.y, 3)},"
-                #         f"{round(pos.z, 3)},{round(spd, 3)},"
-                #         f"{round(cog, 3)},{round(ele, 3)}"
-                #     )
-                # ]
+                pos = rid.enu_pos
+                spd = rid.speed
+                cog = rid.cog
+                ele = rid.ele
+                operands = [
+                    (
+                        f"{sysid},{round(pos.x, 3)},{round(pos.y, 3)},"
+                        f"{round(pos.z, 3)},{round(spd, 3)},"
+                        f"{round(cog, 3)},{round(ele, 3)}"
+                    )
+                ]
                 o_sysids: list[int] = []
                 for o_sysid in self.grid.iter_neighbors_within(
                     sysid, rid.enu_pos, radius=None
                 ):
                     o_rid = self.grid.rid(o_sysid)
                     logging.debug(f"{sysid}({o_rid}) -> {o_sysid} ({o_rid})")
-                    # TODO: get this information with certainty instead of allowing None
                     o_sysids.append(o_sysid)
-                #     o_data = o_rid.data
-                #     o_pos = o_data.enu_pos
-                #     o_spd = o_data.speed
-                #     o_cog = o_data.cog
-                #     o_ele = o_data.ele
-                #     operands.append(
-                #         (
-                #             f"{o_sysid},{round(o_pos.x, 3)},{round(o_pos.y, 3)},"
-                #             f"{round(o_pos.z, 3)},{round(o_spd, 3)},"
-                #             f"{round(o_cog, 3)},{round(o_ele, 3)}"
-                #         )
-                #     )
+                    o_pos = o_rid.enu_pos
+                    o_spd = o_rid.speed
+                    o_cog = o_rid.cog
+                    o_ele = o_rid.ele
+                    operands.append(
+                        (
+                            f"{o_sysid},{round(o_pos.x, 3)},{round(o_pos.y, 3)},"
+                            f"{round(o_pos.z, 3)},{round(o_spd, 3)},"
+                            f"{round(o_cog, 3)},{round(o_ele, 3)}"
+                        )
+                    )
 
-                # # continue if there not at least two drones to simulate
-                # if len(operands) <= 1:
-                #     continue
+                # continue if there not at least two drones to simulate
+                if len(operands) <= 1:
+                    continue
 
-                # # invoke a one-off uli-net-sim Remote ID broadcast simulation
-                # result = subprocess.run(
-                #     [
-                #         "./rid-one-off.sh",
-                #         "-n",
-                #         f"{sysid}",
-                #         # TODO: fill in these RID fields later if needed
-                #         "-t",
-                #         "0",
-                #         "-x",
-                #         "0",
-                #         "-y",
-                #         "0",
-                #         "-z",
-                #         "0",
-                #         "-v",
-                #         "0",
-                #         "-g",
-                #         "0",
-                #         "-h",
-                #         "0",
-                #         "-q",
-                #         "--",
-                #         *operands,
-                #     ],
-                #     cwd="/usr/uli-net-sim",
-                #     capture_output=True,
-                #     text=True,
-                # )
-                # logging.debug(
-                #     f"rid-one-off:\noperands:\n{operands}\n\nstdout:\n{result.stdout}\n\nstderr:\n{result.stderr}"
-                # )
+                # invoke a one-off uli-net-sim Remote ID broadcast simulation
+                result = subprocess.run(
+                    [
+                        "./rid-one-off.sh",
+                        "-n",
+                        f"{sysid}",
+                        # TODO: fill in these RID fields later if needed
+                        "-t",
+                        "0",
+                        "-x",
+                        "0",
+                        "-y",
+                        "0",
+                        "-z",
+                        "0",
+                        "-v",
+                        "0",
+                        "-g",
+                        "0",
+                        "-h",
+                        "0",
+                        "-q",
+                        "--",
+                        *operands,
+                    ],
+                    cwd="/usr/uli-net-sim",
+                    capture_output=True,
+                    text=True,
+                )
+                logging.debug(
+                    f"rid-one-off:\noperands:\n{operands}\n\nstdout:\n{result.stdout}\n\nstderr:\n{result.stderr}"
+                )
 
-                # res = json.loads(result.stdout)
+                res = json.loads(result.stdout)
                 for o_sysid in o_sysids:
-                    # if sysid in res["Serial Number"][o_sysid]["values"]:
-                    self.rid_out_socks[o_sysid].send_pyobj(rid)  # type: ignore
+                    if o_sysid in res["Serial Number"]:
+                        if sysid in res["Serial Number"][o_sysid]["values"]:
+                            self.rid_out_socks[o_sysid].send_pyobj(rid)  # type: ignore
             except Exception as e:
-                logging.error(f"Retransmit error for {sysid}: {e}")
+                logging.error(f"Retransmit error for {sysid} of type {type(e)}: {e}")
             time.sleep(TX_LOOP_SLEEP)
 
     @staticmethod
