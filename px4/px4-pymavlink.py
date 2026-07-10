c35631cb9a00f7e44932a2710e30e5d5da2f19d7
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2026-07-10 16:39:52 -0700

    Remove MavLink Onboard Port as a magic number.

--- px4/px4-pymavlink.py
+++ px4/px4-pymavlink.py
@@ -1,7 +1,9 @@
 from pymavlink import mavutil
 
+ONBOARD_PORT = 14540
+
 # Start a connection listening on a UDP port
-conn = mavutil.mavlink_connection('udpin:localhost:14540')
+conn = mavutil.mavlink_connection(f'udpin:localhost:{ONBOARD_PORT}')
 
 conn.wait_heartbeat()
 print(f"Heartbeat from system {conn.target_system} component {conn.target_component})")
