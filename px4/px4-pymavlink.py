91486fb5edc8259ee5863be07254cc25a56d35a6
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2026-07-10 17:57:29 -0700

    Good progress

--- px4/px4-pymavlink.py
+++ px4/px4-pymavlink.py
@@ -13,11 +13,17 @@ conn = mavutil.mavlink_connection(f'udpin:localhost:{ONBOARD_PORT}')
 conn.wait_heartbeat()
 print(f"Heartbeat from system {conn.target_system} component {conn.target_component})")
 
-# while True:
-#   message = conn.recv_match(blocking=True)
-#   print(message)
-#   time.sleep(SLEEP_TIME_SECS)
-
+# Takeoff command
 conn.mav.command_long_send(conn.target_system, conn.target_component,
                            mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,
                            0, 0, 0, 0, nan, 0, 0, 10)
+
+# Start Mission command
+conn.mav.command_long_send(conn.target_system, conn.target_component,
+                           mavutil.mavlink.MAV_CMD_MISSION_START,
+                           0, 0, 0, 0, 0, 0, 0, 0)
+
+while True:
+  message = conn.recv_match(blocking=True)
+  print(message)
+  time.sleep(SLEEP_TIME_SECS)
