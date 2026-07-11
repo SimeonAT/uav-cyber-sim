6f40d2266c5b4f5f636d897398f705feb7838d78
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2026-07-10 17:17:18 -0700

    Print out mavlink messages every second

--- px4/px4-pymavlink.py
+++ px4/px4-pymavlink.py
@@ -1,7 +1,10 @@
 from pymavlink import mavutil
 import mavtest
 
+import time
+
 ONBOARD_PORT = 14540
+SLEEP_TIME_SECS = 1
 
 # Start a connection listening on a UDP port
 conn = mavutil.mavlink_connection(f'udpin:localhost:{ONBOARD_PORT}')
@@ -9,5 +12,8 @@ conn = mavutil.mavlink_connection(f'udpin:localhost:{ONBOARD_PORT}')
 conn.wait_heartbeat()
 print(f"Heartbeat from system {conn.target_system} component {conn.target_component})")
 
-message = conn.recv_match(blocking=True)
-print(message)
+while True:
+  message = conn.recv_match(blocking=True)
+  print(message)
+
+  time.sleep(SLEEP_TIME_SECS)
