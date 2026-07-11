bae48cee8de7608780c2a9c82be8b5c8025bcdce
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2026-07-10 17:53:21 -0700

    saving code so far

--- px4/px4-pymavlink.py
+++ px4/px4-pymavlink.py
@@ -2,6 +2,7 @@ from pymavlink import mavutil
 import mavtest
 
 import time
+from math import nan
 
 ONBOARD_PORT = 14540
 SLEEP_TIME_SECS = 1
@@ -12,8 +13,11 @@ conn = mavutil.mavlink_connection(f'udpin:localhost:{ONBOARD_PORT}')
 conn.wait_heartbeat()
 print(f"Heartbeat from system {conn.target_system} component {conn.target_component})")
 
-while True:
-  message = conn.recv_match(blocking=True)
-  print(message)
+# while True:
+#   message = conn.recv_match(blocking=True)
+#   print(message)
+#   time.sleep(SLEEP_TIME_SECS)
 
-  time.sleep(SLEEP_TIME_SECS)
+conn.mav.command_long_send(conn.target_system, conn.target_component,
+                           mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,
+                           0, 0, 0, 0, nan, 0, 0, 10)
