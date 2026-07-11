2eee18f7ac20b80d1da101225c97f07e18506151
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2026-07-10 18:07:40 -0700

    Drone takes off, but doesn't fly yet.

--- px4/px4-pymavlink.py
+++ px4/px4-pymavlink.py
@@ -7,23 +7,33 @@ from math import nan
 ONBOARD_PORT = 14540
 SLEEP_TIME_SECS = 1
 
-# Start a connection listening on a UDP port
-conn = mavutil.mavlink_connection(f'udpin:localhost:{ONBOARD_PORT}')
+def send_command(connection, command, confirmation, param1, param2, param3,
+                 param4, param5, param6, param7):
+  return conn.mav.command_long_send(connection.target_system, connection.target_component,
+                                    command, confirmation, param1, param2, param3, param4,
+                                    param5, param6, param7)
 
-conn.wait_heartbeat()
-print(f"Heartbeat from system {conn.target_system} component {conn.target_component})")
+def ack(connection, message_names):
+  ack = connection.recv_match(type=message_names, blocking=True)
+  print(ack)
+  return ack
 
-# Takeoff command
-conn.mav.command_long_send(conn.target_system, conn.target_component,
-                           mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,
-                           0, 0, 0, 0, nan, 0, 0, 10)
+if __name__ == "__main__":
+  # Start a connection listening on a UDP port
+  conn = mavutil.mavlink_connection(f'udpin:localhost:{ONBOARD_PORT}')
 
-# Start Mission command
-conn.mav.command_long_send(conn.target_system, conn.target_component,
-                           mavutil.mavlink.MAV_CMD_MISSION_START,
-                           0, 0, 0, 0, 0, 0, 0, 0)
+  conn.wait_heartbeat()
+  print(f"Heartbeat from system {conn.target_system} component {conn.target_component})")
 
-while True:
-  message = conn.recv_match(blocking=True)
-  print(message)
-  time.sleep(SLEEP_TIME_SECS)
+  # Arm command
+  send_command(conn, mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 1, 0, 0, 0, 0, 0, 0)
+  ack(conn, "COMMAND_ACK")
+
+  # Takeoff command
+  send_command(conn, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,  0, 0, 0, 0, nan, 0, 0, 10)
+  ack(conn, "COMMAND_ACK")
+
+  # while True:
+  #   message = conn.recv_match(blocking=True)
+  #   print(message)
+  #   time.sleep(SLEEP_TIME_SECS)
