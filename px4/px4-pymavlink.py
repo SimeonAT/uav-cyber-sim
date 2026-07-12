f21e0a0ffd636f71f3fb27a28b9ae32c21af3ede
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2026-07-11 21:25:37 -0700

    Good progress

--- px4/px4-pymavlink.py
+++ px4/px4-pymavlink.py
@@ -13,27 +13,18 @@ def send_command(connection, command, confirmation, param1, param2, param3,
                                     command, confirmation, param1, param2, param3, param4,
                                     param5, param6, param7)
 
-def ack(connection, message_names):
-  ack = connection.recv_match(type=message_names, blocking=True)
-  print(ack)
-  return ack
+def print_messages(connection, message_names=None):
+  print(connection.recv_match(type=message_names, blocking=True))
+  return
 
 if __name__ == "__main__":
-  # Start a connection listening on a UDP port
   conn = mavutil.mavlink_connection(f'udpin:localhost:{ONBOARD_PORT}')
 
   conn.wait_heartbeat()
   print(f"Heartbeat from system {conn.target_system} component {conn.target_component})")
 
-  # Arm command
   send_command(conn, mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 1, 0, 0, 0, 0, 0, 0)
-  ack(conn, "COMMAND_ACK")
+  print_messages(conn, message_names="COMMAND_ACK")
 
-  # Takeoff command
   send_command(conn, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,  0, 0, 0, 0, nan, nan, nan, 10)
-  ack(conn, "COMMAND_ACK")
-
-  # while True:
-  #   message = conn.recv_match(blocking=True)
-  #   print(message)
-  #   time.sleep(SLEEP_TIME_SECS)
+  print_messages(conn, message_names="COMMAND_ACK")
