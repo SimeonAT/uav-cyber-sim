c5a186b825297abe91613cd4a66d097525757b88
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2026-07-11 21:39:21 -0700

    Good progress

--- px4/px4-pymavlink.py
+++ px4/px4-pymavlink.py
@@ -13,9 +13,10 @@ def send_command(connection, command, confirmation, param1, param2, param3,
                                     command, confirmation, param1, param2, param3, param4,
                                     param5, param6, param7)
 
-def print_messages(connection, message_names=None):
-  print(connection.recv_match(type=message_names, blocking=True))
-  return
+def print_message(connection, message_names=None):
+  message = connection.recv_match(type=message_names, blocking=True)
+  print(message)
+  return message
 
 if __name__ == "__main__":
   conn = mavutil.mavlink_connection(f'udpin:localhost:{ONBOARD_PORT}')
@@ -24,7 +25,12 @@ if __name__ == "__main__":
   print(f"Heartbeat from system {conn.target_system} component {conn.target_component})")
 
   send_command(conn, mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 1, 0, 0, 0, 0, 0, 0)
-  print_messages(conn, message_names="COMMAND_ACK")
+  print_message(conn, message_names="COMMAND_ACK")
 
-  send_command(conn, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,  0, 0, 0, 0, nan, nan, nan, 10)
-  print_messages(conn, message_names="COMMAND_ACK")
+  send_command(conn, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,  0, 0, 0, 0, nan, nan, nan, 50)
+  print_message(conn, message_names="COMMAND_ACK")
+
+  time.sleep(10)
+
+  send_command(conn, mavutil.mavlink.MAV_CMD_NAV_RETURN_TO_LAUNCH, 0, 0, 0, 0, 0, 0, 0, 0)
+  print_message(conn)
