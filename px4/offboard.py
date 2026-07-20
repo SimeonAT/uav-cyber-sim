df6ec18a13a4b2b6c6825359256a8c1293d0ef9d
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2026-07-20 13:41:51 -0700

    Stream setpoints

--- px4/offboard.py
+++ px4/offboard.py
@@ -1,4 +1,10 @@
 from common import *
+import time
+
+HertzToSeconds = lambda hertz : 1 / hertz
+
+STREAM_RATE_HZ = 20
+STREAM_RATE_SECONDS = HertzToSeconds(STREAM_RATE_HZ)
 
 if __name__ == "__main__":
   conn = mavutil.mavlink_connection(f'udpin:localhost:{ONBOARD_PORT}')
@@ -6,5 +12,8 @@ if __name__ == "__main__":
   conn.wait_heartbeat()
   print(f"Heartbeat from system {conn.target_system} component {conn.target_component}")
 
-  setpoint_send(conn, 0, 0, 0)
-  get_message(conn, message_names="POSITION_TARGET_LOCAL_NED")
+  while True:
+    setpoint_send(conn, 0, 0, 0)
+    get_message(conn, message_names="POSITION_TARGET_LOCAL_NED")
+
+    time.sleep(STREAM_RATE_SECONDS)
