7808012b24b3ea28e370c77badbe7c961d000aeb
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2026-07-20 14:27:40 -0700

    Good progress

--- px4/offboard.py
+++ px4/offboard.py
@@ -7,6 +7,7 @@ STREAM_RATE_HZ = 20
 STREAM_RATE_SECONDS = HertzToSeconds(STREAM_RATE_HZ)
 
 Armed = False
+Mode_Set = False
 
 if __name__ == "__main__":
   conn = mavutil.mavlink_connection(f'udpin:localhost:{ONBOARD_PORT}')
@@ -19,17 +20,25 @@ if __name__ == "__main__":
     elapsed = time.time() - start
 
     setpoint_send(conn, x=0, y=0, z=-2.5)
-    get_message(conn, message_names="POSITION_TARGET_LOCAL_NED", blocking=False, printstd=False)
+    get_message(conn, "POSITION_TARGET_LOCAL_NED", blocking=False, printstd=False)
 
-    if elapsed > 5 and not Armed:
+    if not Mode_Set:
       send_command(conn, mavutil.mavlink.MAV_CMD_DO_SET_MODE, confirmation=0,
-                   param1=mavutil.mavlink.MAV_MODE_FLAG_GUIDED_ENABLED,
-                   param2=0, param3=0, param4=0, param5=0, param6=0, param7=0)
-      get_message(conn, message_names="COMMAND_ACK")
-
+                   param1=209, param2=6, param3=0, param4=0, param5=0, param6=0, param7=0)
+    
+    if not Armed:
       send_command(conn, mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 1, 0, 0, 0, 0, 0, 0)
-      get_message(conn, message_names="COMMAND_ACK")
-      
-      Armed = True
+    
+
+    if not Mode_Set or not Armed:
+      ack = get_message(conn, "COMMAND_ACK", blocking=False, printstd=True)
+      if ack:
+        if ack.command == mavutil.mavlink.MAV_CMD_DO_SET_MODE and \
+          ack.result == mavutil.mavlink.MAV_RESULT_ACCEPTED:
+          Mode_Set = True
+
+        if ack.command == mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM and \
+          ack.result == mavutil.mavlink.MAV_RESULT_ACCEPTED:
+          Armed = True
 
     time.sleep(STREAM_RATE_SECONDS)
