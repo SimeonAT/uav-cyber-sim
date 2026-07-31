ec099b093c8f847bd58537281cae43d191e4a78a
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2026-07-31 12:52:08 -0700

    This line, changing from iris to drone in _add_drone_elements(), causes Gazebo to crash.

--- simulator/visualizer/gazebo/gazebo.py
+++ simulator/visualizer/gazebo/gazebo.py
@@ -231,7 +231,7 @@ class Gazebo(Visualizer[GazVehicle]):
         for i, veh in enumerate(self.vehicles):
             x, y, z, h = veh.home
             pose = XYZRPY(x, y, z, 0, 0, heading_to_yaw(h))
-            drone_elem = self._generate_drone_element(f"drone{i + 1}", pose)
+            drone_elem = self._generate_drone_element(f"iris{i + 1}", pose)
             world_elem.append(drone_elem)
 
     def _add_inertial(self, link: ET.Element) -> None:
