cec143af2236a58756699f637c1c19ff2bd9f300
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2026-07-30 17:47:12 -0700

    There are errors right now, but saving progress on what I'm doing to get uav-cyber-sim to use PX4 drone model instead of ArduPilot Gazebo drone model.

--- simulator/config.py
+++ simulator/config.py
@@ -26,6 +26,11 @@ DATA_PATH = (ROOT / "data").resolve()
 PX4_AUTOPILOT_PATH = HOME / "PX4-Autopilot"
 PX4_BUILD_PATH = PX4_AUTOPILOT_PATH / "build" / "px4_sitl_default"
 
+PX4_GAZEBO_MODELS = (
+  HOME / "PX4-Autopilot" / "Tools" / "simulation"
+  / "gazebo-classic" / "sitl_gazebo-classic" / "models"
+)
+
 PX4_DATA_PATH = (ROOT / "data" / "px4").resolve()
 
 # Ensure logs directory exists (can be cleaned later)
