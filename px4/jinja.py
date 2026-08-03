0c60b93984b6b32e09c066abd0a84b5b5f811860
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2026-08-03 16:38:58 -0700

    Created PX4 template

--- px4/jinja.py
+++ px4/jinja.py
@@ -1,12 +1,16 @@
-import jinja2
+from jinja2 import Environment, FileSystemLoader
 from pathlib import Path
 
 PX4_MODELS = (
-  Path.home() / "PX4-Autopilot" /  "Tools" /  "gazeb-classic" /
-  "sitl_gazebo-classic" / "models"
+  Path.home() / "PX4-Autopilot" /  "Tools" /  "simulation" /
+  "gazebo-classic" / "sitl_gazebo-classic" / "models"
 )
 
 def create_drone_sdf():
+  env = Environment(loader=FileSystemLoader(PX4_MODELS / "iris"))
+  template = env.get_template("iris.sdf.jinja")
+
+  print(template)
   return
 
 if __name__ == "__main__":
