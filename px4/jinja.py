451144406d434ac2f4798cb9a61027ba20d82384
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2026-08-03 16:34:03 -0700

    Good progress

--- px4/jinja.py
+++ px4/jinja.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+import jinja2
+from pathlib import Path
+
+PX4_MODELS = (
+  Path.home() / "PX4-Autopilot" /  "Tools" /  "gazeb-classic" /
+  "sitl_gazebo-classic" / "models"
+)
+
+def create_drone_sdf():
+  return
+
+if __name__ == "__main__":
+  create_drone_sdf()
