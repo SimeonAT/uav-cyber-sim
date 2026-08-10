9fa169ab341f2ff343cbe9d54105b8f3c1a59449
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2026-08-10 15:48:41 -0700

    Saving progress so far.

--- simulator/helpers/connections/mavlink/conn.py
+++ simulator/helpers/connections/mavlink/conn.py
@@ -75,7 +75,10 @@ def create_tcp_conn(
             conn = connect(device_str, src_sysid, src_compid)
             conn.mav.heartbeat_send(sys_type, ardupilot, 0, 0, 0)
 
-            while conn.target_system == 0:
+            if is_client:
+              while conn.target_system == 0:
+                conn.wait_heartbeat()
+            else:
               conn.wait_heartbeat()
 
             return conn
