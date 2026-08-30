55158c82c7f857b9f70f325863ef837b6a82a4a3
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2026-08-30 12:53:09 -0700

    Define macro for setpoint streaming frequency

--- simulator/params/simulation.py
+++ simulator/params/simulation.py
@@ -8,3 +8,10 @@ HEARTBEAT_FREQUENCY: int = 1  # Hz
 DATA_STREAM_FREQUENCY: int = 5  # Hz
 REMOTE_ID_FREQUENCY: int = 5  # Hz
 USE_NETWORK_SIM: bool = False
+
+"""
+To enable offboard (i.e. guided) mode, PX4 requires that setpoints be
+streamed at a frequency >= 2 Hz.
+https://docs.px4.io/main/en/flight_modes/offboard#technical-summary
+"""
+SETPOINT_STREAM_FREQUENCY: int = 2 # Hz
